9ad97ac4b07838a558a540cc037f7933af7697df
yessenia22c <yesseniavillarte@gmail.com>
2024-05-23 11:03:24 -0400

    Oranizacion del componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,28 +1,26 @@
 import { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
 
     const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
         setCategories([newCategory, ...categories ]);
     }
     console.log(categories);
   return (
     <>
-    {/* titulo */}
     <h1>GifExpertApp</h1>
-    {/* Input */}
     <AddCategory 
-        // setCategories={setCategories}
         onNewCategory={(event) => onAddCategory(event)}
     />
-    {/* Listado de gif */}
-    <ol>
-        { categories.map( category => {
-            return <li key={category}>{category}</li>
-        })}
-    </ol>
+    { 
+        categories.map( category => (
+            <GifGrid key={category} category={category}/>
+        ))
+    }
     </>
   )
 }
